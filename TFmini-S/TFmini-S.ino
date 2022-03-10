c1fb14c991c93501fead865de1ceb0ea8674f416
ISMA-FH <101289917+ISMA-FH@users.noreply.github.com>
2022-03-10 10:39:35 -0600

    Programa Nuevo

---
+++ TFmini-S/TFmini-S.ino
@@ -0,0 +1,14 @@
+//////////////************Progama para el funcionamiento del sensor TFmini-S  LIDAR***************/////////////////
+/////////////10 de Marza 2022/////////////////////
+
+void setup()
+{
+
+
+}
+
+void loop()
+{
+
+
+}
